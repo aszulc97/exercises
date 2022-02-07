25ec39082057b45cdc17963dc155801da10753aa
Agata Szulc <agataszulc97@gmail.com>
2022-02-07 16:24:04 +0100

    dunno

--- capitalization3.js
+++ capitalization3.js
@@ -1,4 +1,4 @@
-let str = "peter";
+let str = "PEtER";
 document.getElementById("before").textContent = str;
 document.getElementById("after").textContent =
   str.substring(0, 2).toLowerCase() + str.charAt(2).toUpperCase() + str.substring(3).toLowerCase();
