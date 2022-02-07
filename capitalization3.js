b7dc7375c00290c4751d4808d47f2a0069302f65
Agata Szulc <agataszulc97@gmail.com>
2022-02-07 16:21:58 +0100

    3caPitalization exercise

---
+++ capitalization3.js
@@ -0,0 +1,4 @@
+let str = "peter";
+document.getElementById("before").textContent = str;
+document.getElementById("after").textContent =
+  str.substring(0, 2).toLowerCase() + str.charAt(2).toUpperCase() + str.substring(3).toLowerCase();
