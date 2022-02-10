4eb36916c71b3616fd64966820f133ee983b7101
Agata Szulc <agataszulc97@gmail.com>
2022-02-10 10:35:09 +0100

    for loop exercises

---
+++ for-loop.js
@@ -0,0 +1,35 @@
+//from 0 to 9
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+//from 1 to 10
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+//10 to 0 and after 0 "liftoff"
+// for (let i = 10; i >= 0; i--) {
+//   console.log(i);
+// }
+// console.log("liftoff");
+
+//from 1 to 19 but only odd
+// for (let i = 1; i < 20; i += 2) {
+//   console.log(i);
+// }
+
+//from 1 to 16777216 but doubling
+// for (let i = 1; i < 16777217; i *= 2) {
+//   console.log(i);
+// }
+
+//from 111 to 138 but in steps of 3
+// for (let i = 111; i < 139; i += 3) {
+//   console.log(i);
+// }
+
+//from 100 to 0 but in steps of 10
+for (let i = 100; i >= 0; i -= 10) {
+  console.log(i);
+}
