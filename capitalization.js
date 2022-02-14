9814e821d55dfc35fc68882952abca9dd95b0238
Agata Szulc <agataszulc97@gmail.com>
2022-02-14 14:34:38 +0100

    some exercise

--- capitalization.js
+++ capitalization.js
@@ -2,3 +2,5 @@ let str = "PEtER";
 document.getElementById("before").textContent = str;
 document.getElementById("after").textContent =
   str.charAt(0).toUpperCase() + str.substring(1).toLowerCase();
+
+functio;
