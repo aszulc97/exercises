46d8c3552ee5ea5b0c2fca89b8c85080384cfeb0
Agata Szulc <agataszulc97@gmail.com>
2022-02-14 15:23:39 +0100

    fullname function

--- nameparts.js
+++ nameparts.js
@@ -1,11 +1,25 @@
-let str = "Albus Percival Wulfric Brian Dumbledore";
-str = "Agata Szulc";
+// let str = "Albus Percival Wulfric Brian Dumbledore";
+// str = "Agata Szulc";
 
-function getNameParts(fullname) {
-  let firstname = fullname.substring(0, fullname.indexOf(" "));
-  let middlename = fullname.substring(fullname.indexOf(" ") + 1, fullname.lastIndexOf(" "));
-  let lastname = fullname.substring(fullname.lastIndexOf(" ") + 1, fullname.length);
-  console.log({ firstname, middlename, lastname });
+// function getNameParts(fullname) {
+//   let firstname = fullname.substring(0, fullname.indexOf(" "));
+//   let middlename = fullname.substring(fullname.indexOf(" ") + 1, fullname.lastIndexOf(" "));
+//   let lastname = fullname.substring(fullname.lastIndexOf(" ") + 1, fullname.length);
+//   console.log({ firstname, middlename, lastname });
+// }
+
+// getNameParts(str);
+
+function fullName(lastname, firstname, middlename) {
+  let fullnameString;
+  if (middlename) {
+    fullnameString = firstname + " " + middlename + " " + lastname;
+  } else {
+    fullnameString = firstname + " " + lastname;
+  }
+  console.log(fullnameString);
 }
 
-getNameParts(str);
+fullName("Potter", "Harry", "James");
+fullName("Potter", "Harry");
+fullName("Potter", "Harry", "James", "Pottypotpot");
