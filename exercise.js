7715a5a0492450c797d5fead5025ebd0850ed36c
Agata Szulc <agataszulc97@gmail.com>
2022-02-14 14:34:50 +0100

    some exercise

---
+++ exercise.js
@@ -0,0 +1,13 @@
+// sayHello("agatka");
+
+// function sayHello(firstName) {
+//   console.log("Hello ", firstName);
+// }
+
+let a = 3;
+
+function sum(a, b) {
+  console.log(a + b);
+}
+
+sum(4, 5);
