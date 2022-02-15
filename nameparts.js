e1a7e46669e614ed5bbd24885b6b6887c12ca82e
Agata Szulc <agataszulc97@gmail.com>
2022-02-15 12:25:59 +0100

    another way to do capitalization

--- nameparts.js
+++ nameparts.js
@@ -4,6 +4,11 @@ let str = "Albus PerciVAl WULFRIC Brian DumbledoRE";
 function getNameParts(fullname) {
   let firstname = capitalize(fullname.substring(0, fullname.indexOf(" ")));
   let middlename = capitalize(fullname.substring(fullname.indexOf(" ") + 1, fullname.lastIndexOf(" ")));
+  let middlenameArray = middlename.split(" ");
+  for (let i = 0; i < middlenameArray.length; i++) {
+    middlenameArray[i] = capitalize(middlenameArray[i]);
+  }
+  middlename = middlenameArray.join(" ");
   let lastname = capitalize(fullname.substring(fullname.lastIndexOf(" ") + 1, fullname.length));
   return { firstname, middlename, lastname };
 }
@@ -26,20 +31,20 @@ console.log(answer);
 // fullName("Potter", "Harry");
 // fullName("Potter", "Harry", "James", "Pottypotpot");
 
-// function capitalize(string) {
-//   string = string.charAt(0).toUpperCase() + string.substring(1).toLowerCase();
-//   return string;
-// }
-
-function capitalize(dataString) {
-  let charArray = dataString.toLowerCase().split("");
-  charArray[0] = charArray[0].charAt(0).toUpperCase();
-
-  for (let i = 0; i < charArray.length + 1; i++) {
-    if (charArray[i] === " " || charArray[i] === "-" || charArray[i] === '"') {
-      charArray[i + 1] = charArray[i + 1].charAt(0).toUpperCase();
-    }
-  }
-  console.log(charArray.join(""));
-  return charArray.join("");
+function capitalize(string) {
+  string = string.charAt(0).toUpperCase() + string.substring(1).toLowerCase();
+  return string;
 }
+
+// function capitalize(dataString) {
+//   let charArray = dataString.toLowerCase().split("");
+//   charArray[0] = charArray[0].charAt(0).toUpperCase();
+
+//   for (let i = 0; i < charArray.length + 1; i++) {
+//     if (charArray[i] === " " || charArray[i] === "-" || charArray[i] === '"') {
+//       charArray[i + 1] = charArray[i + 1].charAt(0).toUpperCase();
+//     }
+//   }
+//   console.log(charArray.join(""));
+//   return charArray.join("");
+// }
