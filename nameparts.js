84a40b7cdbd7b59cc705b5d8267cc70bdf3a9110
Agata Szulc <agataszulc97@gmail.com>
2022-02-15 12:22:41 +0100

    nameparts now takes a fullname string and fix capi

--- nameparts.js
+++ nameparts.js
@@ -1,27 +1,45 @@
-// let str = "Albus Percival Wulfric Brian Dumbledore";
-// str = "Agata Szulc";
+let str = "Albus PerciVAl WULFRIC Brian DumbledoRE";
+//str = "Agata Szulc";
 
-// function getNameParts(fullname) {
-//   let firstname = fullname.substring(0, fullname.indexOf(" "));
-//   let middlename = fullname.substring(fullname.indexOf(" ") + 1, fullname.lastIndexOf(" "));
-//   let lastname = fullname.substring(fullname.lastIndexOf(" ") + 1, fullname.length);
-//   console.log({ firstname, middlename, lastname });
-// }
-
-// getNameParts(str);
-
-function fullName(lastname, firstname, middlename) {
-  let fullnameString;
-  if (middlename) {
-    fullnameString = firstname + " " + middlename + " " + lastname;
-  } else {
-    fullnameString = firstname + " " + lastname;
-  }
-  return fullnameString;
+function getNameParts(fullname) {
+  let firstname = capitalize(fullname.substring(0, fullname.indexOf(" ")));
+  let middlename = capitalize(fullname.substring(fullname.indexOf(" ") + 1, fullname.lastIndexOf(" ")));
+  let lastname = capitalize(fullname.substring(fullname.lastIndexOf(" ") + 1, fullname.length));
+  return { firstname, middlename, lastname };
 }
 
-//let answer = fullName("Potter", "Harry", "James", "Pottypotpot");
-let answer = fullName("Szulc", "Agata");
+let answer = getNameParts(str);
 console.log(answer);
+// function fullName(lastname, firstname, middlename) {
+//   let fullnameString;
+//   if (middlename) {
+//     fullnameString = firstname + " " + middlename + " " + lastname;
+//   } else {
+//     fullnameString = firstname + " " + lastname;
+//   }
+//   return fullnameString;
+// }
+
+//let answer = fullName("Potter", "Harry", "James", "Pottypotpot");
+// let answer = fullName("Szulc", "Agata");
+// console.log(answer);
 // fullName("Potter", "Harry");
 // fullName("Potter", "Harry", "James", "Pottypotpot");
+
+// function capitalize(string) {
+//   string = string.charAt(0).toUpperCase() + string.substring(1).toLowerCase();
+//   return string;
+// }
+
+function capitalize(dataString) {
+  let charArray = dataString.toLowerCase().split("");
+  charArray[0] = charArray[0].charAt(0).toUpperCase();
+
+  for (let i = 0; i < charArray.length + 1; i++) {
+    if (charArray[i] === " " || charArray[i] === "-" || charArray[i] === '"') {
+      charArray[i + 1] = charArray[i + 1].charAt(0).toUpperCase();
+    }
+  }
+  console.log(charArray.join(""));
+  return charArray.join("");
+}
